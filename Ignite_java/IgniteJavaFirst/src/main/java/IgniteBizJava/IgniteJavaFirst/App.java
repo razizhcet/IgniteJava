05482d4158cae681739391f39dbc327b42fa3f3a
Razi Ahmad <razizhcet@gmail.com>
2018-04-16 19:54:05 +0530

    ignite java

---
+++ Ignite_java/IgniteJavaFirst/src/main/java/IgniteBizJava/IgniteJavaFirst/App.java
@@ -0,0 +1,13 @@
+package IgniteBizJava.IgniteJavaFirst;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
