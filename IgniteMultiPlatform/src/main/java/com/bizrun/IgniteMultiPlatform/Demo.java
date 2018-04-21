b89fdce18448e54e67a8d4bf80003c5f0abc9fbf
Razi Ahmad <razizhcet@gmail.com>
2018-04-21 19:16:23 +0530

    ignite java final

---
+++ IgniteMultiPlatform/src/main/java/com/bizrun/IgniteMultiPlatform/Demo.java
@@ -0,0 +1,59 @@
+package com.bizrun.IgniteMultiPlatform;
+
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteAtomicSequence;
+import org.apache.ignite.IgniteCache;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.binary.BinaryBasicIdMapper;
+import org.apache.ignite.binary.BinaryBasicNameMapper;
+import org.apache.ignite.cache.query.ContinuousQuery;
+import org.apache.ignite.configuration.BinaryConfiguration;
+import org.apache.ignite.configuration.IgniteConfiguration;
+
+import javax.cache.event.CacheEntryEvent;
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        // Retrieve user name
+        System.out.print("Hi, enter your name: ");
+        Scanner consoleScanner = new Scanner(System.in);
+        String name = consoleScanner.nextLine();
+
+        // Configure Ignite to connect with .NET nodes
+        BinaryConfiguration binCfg = new BinaryConfiguration();
+
+        binCfg.setIdMapper(new BinaryBasicIdMapper());
+        binCfg.setNameMapper(new BinaryBasicNameMapper());
+
+        IgniteConfiguration cfg = new IgniteConfiguration().setBinaryConfiguration(binCfg);
+
+        // Start Ignite and retrieve cache
+        Ignite ignite = Ignition.start(cfg);
+        IgniteCache<Long, Message> cache = ignite.getOrCreateCache("chat");
+
+        // Initialize unique ID sequence
+        IgniteAtomicSequence messageId = ignite.atomicSequence("chatId", 0, true);
+
+        // Set up continuous query
+        ContinuousQuery<Long, Message> qry = new ContinuousQuery<>();
+
+        qry.setLocalListener(iterable -> {
+            // This will be invoked immediately on each cache update
+            for (CacheEntryEvent<? extends Long, ? extends Message> evt : iterable)
+                System.out.println(evt.getValue().author + ": " + evt.getValue().text);
+        });
+
+        cache.query(qry);
+
+        // Run the chat loop
+        while (true) {
+            System.out.print("> ");
+
+            String msgText = consoleScanner.nextLine();
+            Long msgId = messageId.incrementAndGet();
+
+            cache.put(msgId, new Message(name, msgText));
+        }
+    }
+}
---
+++ IgniteMultiPlatform/src/main/java/com/bizrun/IgniteMultiPlatform/Message.java
@@ -0,0 +1,11 @@
+package com.bizrun.IgniteMultiPlatform;
+
+public class Message {
+    public Message(String author, String text) {
+        this.author = author;
+        this.text = text;
+    }
+
+    final String author;
+    final String text;
+}
